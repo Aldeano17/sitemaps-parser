655fcb1a6a6ec5d354e51d86f459c88f432751b6
Adrian Marimon <adrianmarimon1@gmail.com>
2019-06-22 23:51:02 -0400

    added gitignore

---
+++ README.md
@@ -0,0 +1,12 @@
+# Objective
+- Save all urls in a domain to a Google Cloud datatabse
+- Run with `python3 main.py https://www.example.com/sitemap.xml`
+# Steps
+2. Visit sitemap
+3. Parse sitemap XML to an object in python
+4. Save python object to database
+
+### Dependencies
+requests
+xmltodict
+sys
---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import xmltodict
+import sys
+from google.cloud import firestore
+
+def save_to_db(obj):
+    db = firestore.Client("futbooool-1530472667061")
+
+    doc_reference = db.collection(u'sitemap').document()
+    doc_reference.set(obj)
+
+def get_host(url):
+    return url.split("//")[1].split("/")[0]
+
+def get_name(url):
+    return url.split(".")[1]
+
+if len(sys.argv) < 2:
+    print("not enough arguments")
+    exit()
+
+sitemapurl = sys.argv[1]
+
+
+response = requests.get(sitemapurl)
+list = xmltodict.parse(response.text)
+
+
+list2 = []
+for e in list["urlset"]["url"]:
+    obj = {
+        "url": e["loc"],
+        "udpated": e["lastmod"]
+    }
+
+    list2.append(obj)
+    break
+
+obj = {
+    "host": get_host(sitemapurl),
+    "name": get_name(sitemapurl),
+    "size": len(list2),
+    "urls": list2
+}
+
+
+save_to_db(obj)
+
+print(obj)
